98763aa7999d70c7708f15201f5f3b5e01410b54
Eric Williams <ewilliams0305@gmail.com>
2025-08-04 19:53:39 -0400

    added hotpath method

---
+++ example/Scribbly.Stencil.Cookbook.Api/Endpoints/Menu/Test.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scribbly.Stencil.Cookbook.ApiService.Endpoints.Menu;
+
+
+public static partial class NoGroupEndpoints
+{
+
+    // ----------------------------------------> THE CODE USER WILL WRITE
+    
+    [GetEndpoint("no-group/{id}")]
+    private static IResult GetLunchMenu(HttpContext context, [FromRoute] string id)
+    {
+        return Results.Ok(id);
+    }
+    static partial void ConfigureGetLunchMenu(IEndpointConventionBuilder builder)
+    {
+        builder
+            .WithRequestTimeout(TimeSpan.FromSeconds(1))
+            .ProducesProblem(400).WithDisplayName("DISPLAY NAME");
+    }
+    
+    [PostEndpoint("no-group/{id}")]
+    private static object PostLunchMenu(HttpContext context, [FromRoute] string id)
+    {
+        return Results.Ok(id);
+    }
+    
+    [PutEndpoint("no-group/{id}")]
+    private static object PutLunchMenu(HttpContext context, [FromRoute] string id)
+    {
+        return new { id = id };
+    }
+}
