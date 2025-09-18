4fb743b5324efac16b970f8b8456ca407a98091f
Eric Williams <ewilliams0305@gmail.com>
2025-09-18 11:23:35 -0400

    leveraged records for value equality

--- source/Scribbly.Stencil.Generator/Groups/Context/TargetGroupCaptureContext.cs
+++ source/Scribbly.Stencil.Generator/Groups/Context/TargetGroupCaptureContext.cs
@@ -1,6 +1,6 @@
 ﻿namespace Scribbly.Stencil.Groups;
 
-public class TargetGroupCaptureContext : IComparable<TargetGroupCaptureContext>, IEquatable<TargetGroupCaptureContext>
+public record TargetGroupCaptureContext
 {
     public string? Namespace { get; }
     public string? TypeName { get; }
@@ -24,33 +24,5 @@ public class TargetGroupCaptureContext : IComparable<TargetGroupCaptureContext>,
         Tag = tag;
         MemberOf = memberOf;
         IsConfigurable = isConfigurable;
-    }
-
-    public int CompareTo(TargetGroupCaptureContext? other)
-    {
-        if(other == null) return -1;
-
-        if (other.Namespace != Namespace) return -1;
-        if (other.TypeName != TypeName) return -1;
-        if (other.RoutePrefix != RoutePrefix) return -1;
-        if (other.Tag != Tag) return -1;
-        if (other.MemberOf != MemberOf) return -1;
-        if (other.IsConfigurable != IsConfigurable) return -1;
-        
-        return 0;
-    }
-
-    public bool Equals(TargetGroupCaptureContext? other)
-    {
-        if (other == null) return false;
-
-        if (other.Namespace != Namespace) return false;
-        if (other.TypeName != TypeName) return false;
-        if (other.RoutePrefix != RoutePrefix) return false;
-        if (other.Tag != Tag) return false;
-        if (other.MemberOf != MemberOf) return false;
-        if (other.IsConfigurable != IsConfigurable) return false;
-       
-        return true;
     }
 }
