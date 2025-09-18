08b20818b6c93dc9ed0001e4fae10545a4cd9955
Eric Williams <ewilliams0305@gmail.com>
2025-09-18 07:21:50 -0400

    created simple record to capture all handler contexts

---
+++ source/Scribbly.Stencil.Generator/Endpoints/Context/CapturedHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Scribbly.Stencil.Endpoints;
+
+internal record CapturedHandler(
+    string Namespace,
+    string ClassName,
+    string MethodName,
+    string HttpVerb,
+    string? Route,
+    string? Name,
+    string? Description,
+    string? MemberOf,
+    bool IsEndpointGroup,
+    TargetMethodCaptureContext.DeclarationMode ConfigurationMode,
+    TargetMethodCaptureContext.DeclarationMode GroupMode
+);
+
+internal class CapturedHandlerComparer : IEqualityComparer<CapturedHandler?>
+{
+    private static readonly Lazy<CapturedHandlerComparer> Lazy = new(() => new CapturedHandlerComparer());
+    public static CapturedHandlerComparer Instance => Lazy.Value;
+
+    public bool Equals(CapturedHandler? x, CapturedHandler? y)
+    {
+        if (ReferenceEquals(x, y)) return true;
+        if (ReferenceEquals(x, null)) return false;
+        if (ReferenceEquals(y, null)) return false;
+        if (x.GetType() != y.GetType()) return false;
+
+        return x.Equals(y);
+    }
+
+    public int GetHashCode(CapturedHandler? obj)
+    {
+        return obj?.GetHashCode() ?? 0;
+    }
+}
