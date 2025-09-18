b9816cd1244c22c5ca3e7e7a48d7db048fdc6b98
Eric Williams <ewilliams0305@gmail.com>
2025-09-18 07:21:10 -0400

    added support for records

---
+++ source/Scribbly.Stencil.Generator/IsExternalInit.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Adds support for init only properties and records.
+    /// </summary>
+    public class IsExternalInit { }
+}
