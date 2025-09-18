87840c595f8515f7c739793a269eacc5cffbb6a9
Eric Williams <ewilliams0305@gmail.com>
2025-09-18 09:49:12 -0400

    created extensions used to query and parse data from known Attributes

---
+++ source/Scribbly.Stencil.Generator/Shared/ITypeSymbolExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Scribbly.Stencil.Extensions;
+
+/// <summary>
+/// Extensions to query and search Typed Symbols
+/// </summary>
+internal static class ITypeSymbolExtensions
+{
+    /// <summary>
+    /// True when an ITypedSymbol is a Configure Attribute
+    /// </summary>
+    /// <param name="typeSymbol">The symbol to query</param>
+    /// <returns>True when the Attribute is a ConfigureAttribute</returns>
+    public static bool IsConfigurationAttribute(this ITypeSymbol? typeSymbol) =>
+        typeSymbol is INamedTypeSymbol
+        {
+            ContainingNamespace:
+            {
+                Name: "Stencil",
+                ContainingNamespace:
+                {
+                    Name: "Scribbly",
+                    ContainingNamespace.IsGlobalNamespace: true,
+                },
+            },
+            Name: "ConfigureAttribute",
+        }
+        || (typeSymbol?.BaseType is { } bt && IsConfigurationAttribute(bt)); 
+    
+    /// <summary>
+    /// True when an ITypedSymbol is an Endpoint Group Attribute
+    /// </summary>
+    /// <param name="typeSymbol">The symbol to query</param>
+    /// <returns>True when the Attribute is a EndpointGroupAttribute</returns>
+    public static bool IsEndpointGroupAttribute(this ITypeSymbol? typeSymbol) =>
+        typeSymbol is INamedTypeSymbol
+        {
+            ContainingNamespace:
+            {
+                Name: "Stencil",
+                ContainingNamespace:
+                {
+                    Name: "Scribbly",
+                    ContainingNamespace.IsGlobalNamespace: true,
+                },
+            },
+            Name: "EndpointGroupAttribute",
+        }
+        || (typeSymbol?.BaseType is { } bt && IsConfigurationAttribute(bt));
+
+    /// <summary>
+    /// True when an ITypedSymbol is a Group Member Attribute
+    /// </summary>
+    /// <param name="typedSymbol">The symbol to query</param>
+    /// <param name="typeArgument">The type symbol passed into the Group Member generic parameter.</param>
+    /// <returns>True when the Attribute is a EndpointGroupAttribute</returns>
+    public static bool IsGroupMemberAttribute(this ITypeSymbol? typedSymbol, out INamedTypeSymbol? typeArgument)
+    {
+        if (typedSymbol is INamedTypeSymbol 
+            {
+                ContainingNamespace:
+                {
+                    Name: "Stencil",
+                    ContainingNamespace:
+                    {
+                        Name: "Scribbly",
+                        ContainingNamespace.IsGlobalNamespace: true,
+                    },
+                },
+                Name: "GroupMemberAttribute",
+                Arity: 1,
+                TypeArguments.Length: 1
+            } typeSymbol && typeSymbol.TypeArguments[0] is INamedTypeSymbol genericTypeArg)
+        {
+            typeArgument = genericTypeArg;
+            return true;
+        }
+
+        typeArgument = null;
+        return false;
+    }
+}
